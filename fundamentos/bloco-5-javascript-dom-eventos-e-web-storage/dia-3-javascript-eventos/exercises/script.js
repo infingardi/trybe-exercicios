1a07e32ac8bb787e832b8563063ec445cd0b0663
igor <infingardidev@gmail.com>
2021-10-21 19:21:26 -0300

    terminando exercicios até o 10

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/script.js
@@ -17,17 +17,17 @@ createDaysOfTheWeek();
 function createDaysOnCalendary() {
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     let listaDeDias = document.querySelector('#days');
-    
+
     for (const day of dezDaysList) {
         let dia = document.createElement('li');
         dia.classList.add('day');
         dia.textContent = day;
 
-        if(day == 24 || day === 25 || day === 31){
+        if (day == 24 || day === 25 || day === 31) {
             dia.classList.add('holiday');
         }
 
-        if(day == 4 || day === 11 || day === 18 || day == 25){
+        if (day == 4 || day === 11 || day === 18 || day == 25) {
             dia.classList.add('friday');
         }
         listaDeDias.appendChild(dia);
@@ -57,9 +57,9 @@ btn.addEventListener('click', mudaCor);
 
 function mudaCor() {
     let feriados = document.querySelectorAll('.holiday');
-    let elementoCor = feriados[0].style.backgroundColor; 
+    let elementoCor = feriados[0].style.backgroundColor;
 
-    if(elementoCor == 'green'){
+    if (elementoCor == 'green') {
         elementoCor = 'rgb(238,238,238)';
     } else {
         elementoCor = 'green';
@@ -91,13 +91,11 @@ for (const day of sextas) {
     fridayContent.push(day.textContent);
 }
 
-console.log(fridayContent);
-
 btnFriday.addEventListener('click', modificaTexto);
 function modificaTexto() {
     let i = 0;
     for (const sexta of sextas) {
-        if(sexta.textContent == 'sextou'){
+        if (sexta.textContent == 'sextou') {
             sexta.textContent = fridayContent[i];
             i++;
         } else {
@@ -105,6 +103,90 @@ function modificaTexto() {
         }
     }
 }
+
+// exercicio 6
+
+let listaUl = document.querySelector('#days');
+
+listaUl.addEventListener('mouseover', function (event) {
+    if (event.target.id !== 'days') {
+        event.target.style.fontSize = '1.5em';
+    }
+});
+
+listaUl.addEventListener('mouseout', function (event) {
+    if (event.target.id !== 'days') {
+        event.target.style.fontSize = '20px';
+    }
+});
+
+// exercicio 7
+
+function createTask(task) {
+    let taskTag = document.createElement('span');
+    taskTag.textContent = task;
+
+    let container = document.querySelector('.my-tasks');
+    container.appendChild(taskTag);
+}
+
+createTask('cozinhar');
+
+// exercicio 8
+
+function criaLegendaTask(cor) {
+    let myDiv = document.createElement('div');
+    myDiv.classList.add('task');
+    myDiv.style.backgroundColor = cor;
+
+    let container = document.querySelector('.my-tasks');
+    container.appendChild(myDiv);
+}
+
+criaLegendaTask('red');
+
+// exercicio 9
+
+function selecionaDiv() {
+    let todasTasks = document.querySelectorAll('.task');
+
+    for (const task of todasTasks) {
+        task.addEventListener('click', function () {
+            if (task.classList[1] === 'selected') {
+                task.classList.remove('selected');
+            } else {
+                task.classList.add('selected');
+            }
+            console.log(task.classList);
+        });
+    }
+}
+
+selecionaDiv();
+
+
+// exercicio 10
+
+function mudaCorDiaDoMes() {
+    let listaUl = document.querySelector('#days');
+
+
+    listaUl.addEventListener('click', function (event) {
+        if (document.querySelector('.selected')) {
+            let taskSelectedColor = document.querySelector('.selected').style.backgroundColor;
+            if (event.target.id !== 'days') {
+                if (taskSelectedColor === event.target.style.color) {
+                    event.target.style.color = 'rgb(119,119,119)';
+                } else {
+                    event.target.style.color = taskSelectedColor;
+                }
+            }
+        }
+    });
+
+}
+
+mudaCorDiaDoMes();
 
 
 
